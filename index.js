8d2fa5d43699a7141a7660218a784d7457f59e2f
icancodebeauty <adi@mail.io>
2022-01-27 15:36:08 +0000

    🏁 Made Finally!.

--- index.js
+++ index.js
@@ -1,87 +1,38 @@
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const app = express();
-
-// const newspapers = [
-//   {
-//     name: "cityam",
-//     address: "https://newsnodes.com/omicron_tracker/",
-//     base: "",
-//   },
-// ];
-
-const articles = [];
-
-// newspapers.forEach((newspaper) => {
+let countries = [];
+let cases = [];
+let finaljson = [];
+// Axios Get Request!!
 axios.get("https://newsnodes.com/omicron_tracker/").then((response) => {
   const html = response.data;
-  // console.log(html);
   const $ = cheerio.load(html);
-
-  // $("tbody", html).each(function () {
-  // console.log(this);
-  // console.log(this.name);
-  // const newhtml = this;
-  // articles.push(newhtml);
-
-  // });
-  // });
-  // });
-  // get country
-  // var countries = [];
-  // $("#datatab a").each(function () {
-  //   countries.push($(this).text().trim());
-  // });
-  // // console.log(countries);
-  // const fcountry = countries.filter((country) => {
-  //   return country != "source";
-  // });
-  // get cases
-  var cases = [];
-  $("#datatab .u-text-r").each(function () {
-    cases.push($(this).text().trim());
+  // Get Country!!
+  $("#datatab tr").each(function () {
+    countries.push($(this).children("td").first().text().trim());
   });
-  const fcases = cases.filter((cse) => {
-    //cse short for case because case is taken
-    return cse != "" && cse != "-" && !cse.includes("%") && cse !="source" && !cse.includes("(");
+  // Get Cases!!
+  $("#datatab tr").each(function () {
+    cases.push($(this).children("td").next().first().text().trim());
   });
-
+  // Create A JSON From Arrays!!
+  const numCountries = countries.length;
+  // Loop Through Number Of Countries
+  for (let index = 0; index < numCountries; index++) {
+    // Object Create
+    let object = {
+      countryName: countries[index],
+      noOfCase: cases[index],
+    };
+    finaljson.push(object);
+  }
+  // Create A `/` Endpoint
   app.get("/", (req, res) => {
-    // res.send(fcountry.slice(0, 150));
-    res.send(fcases);
+    res.send(finaljson);
   });
-
-  // app.get("/news/:newspaperId", (req, res) => {
-  //   const newspaperId = req.params.newspaperId;
-
-  //   const newspaperAddress = newspapers.filter(
-  //     (newspaper) => newspaper.name == newspaperId
-  //   )[0].address;
-  //   const newspaperBase = newspapers.filter(
-  //     (newspaper) => newspaper.name == newspaperId
-  //   )[0].base;
-
-  //   axios
-  //     .get(newspaperAddress)
-  //     .then((response) => {
-  //       const html = response.data;
-  //       const $ = cheerio.load(html);
-  //       const specificArticles = [];
-
-  //       $('a:contains("climate")', html).each(function () {
-  //         const title = $(this).text();
-  //         const url = $(this).attr("href");
-  //         specificArticles.push({
-  //           title,
-  //           url: newspaperBase + url,
-  //           source: newspaperId,
-  //         });
-  //       });
-  //       res.json(specificArticles);
-  //     })
-  //     .catch((err) => console.log(err));
 });
-
+// Listen App!!
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
